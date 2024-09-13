c81218aba384e142dc01387cc59fb992c2e7a3ea
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-09-13 23:33:33 +0530

    made user model and created a test api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';  // Include .js extension
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO)
+  .then(() => {
     console.log("connected to db");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("running on port 3000");
+app.use("/api/user", userRouter);  // Path for user routes
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
