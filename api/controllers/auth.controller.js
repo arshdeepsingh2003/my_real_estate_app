341f8e58d44a406845b510d437f0c8319522398d
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-10-11 20:22:15 +0530

    made signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
     const hashedPassword=bcryptjs.hashSync(password,10);
@@ -15,3 +17,24 @@ export const signup=async(req,res,next)=>{
     
 
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User Not Found!'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET); // Ensure JWT_SECRET is set in your environment variables
+        const {password: pass,...rest}=validUser._doc;//seperate password from rest part so that it is not displayyed to user
+        res.cookie('access_token', token, { httpOnly: true }) // Changed cookie name to 'access_token'
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
+
+
