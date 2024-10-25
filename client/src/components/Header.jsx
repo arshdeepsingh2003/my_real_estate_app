062e61a3845b8318115ebbb03a0b64d5dfc94eb7
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2024-10-25 21:29:07 +0530

    complete header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,26 @@
+import { useState,useEffect } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
   const { currentUser } = useSelector(state => state.user);
-
+  const[searchTerm,setSearchTerm]=useState('');
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-slate-200 show-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,13 +30,18 @@ const Header = () => {
             <span className="text-slate-800">Estate</span>
           </h1>
         </a>
-        <form className="bg-slate-100 p-3 rounded-r-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-r-lg flex items-center">
           <input 
             type="text" 
             placeholder="search..." 
-            className="bg-transparent focus:outline-none w-24 sm:w-64" 
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e)=>setSearchTerm(e.target.value)}
           />
+          <button>
           <FaSearch className="text-slate-600" />
+          </button>
+          
         </form>
         <ul className="flex gap-4">
           <a href="/">
