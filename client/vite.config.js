9ae69d020d01809e65e2f54387a2f49a5559f8e9
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-02-05 20:09:26 +0530

    added vercel linkof backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-  port:3001,  
-  proxy:{
-    '/api':
-    {target:'http://localhost:3000',
-    secure:false,
+  server: {
+    port: 3001,
+    proxy: {
+      '/api': {
+        target: 'https://my-real-estate-app-pyjg.vercel.app/', 
+        changeOrigin: true,
+        secure: true,
+      },
     },
   },
-},
   plugins: [react()],
-})
+});
