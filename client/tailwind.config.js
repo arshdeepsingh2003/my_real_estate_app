e0ff76729c95478427b9c8e823c8d729f87ac8c0
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-02-10 08:54:42 +0530

    added render backurl

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],  
-  theme: { extend: {} },
-  plugins: [],
-};
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+// https://vitejs.dev/config/
+export default defineConfig({
+  server:{
+  port:3001,  
+  proxy:{
+    '/api':
+    {target:'https://my-real-estate-app.onrender.com',
+    secure:false,
+    },
+  },
+},
+  plugins: [react()],
+})
+
