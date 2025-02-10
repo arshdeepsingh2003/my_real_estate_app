c236c59de2c67d7c8991bcc65e34520e9c61863a
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-02-10 08:57:58 +0530

    removed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   port:3001,  
   proxy:{
     '/api':
-    {target:'https://my-real-estate-app.onrender.com',
+    {target:'http://localhost:3000',
     secure:false,
     },
   },
