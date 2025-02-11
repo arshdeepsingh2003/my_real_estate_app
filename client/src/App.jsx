7288af4823e97d86c0ac8287df2ad209d35a7e9f
arshdeepsingh2003 <arshdeepgtbit@gmail.com>
2025-02-11 17:02:03 +0530

    updated app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,27 +10,31 @@ import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
 import Search from './pages/Search';
+import Footer from './pages/Footer';
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/listing/:listingId' element={<Listing />} />
-        <Route path='/search' element={<Search />} />
-        <Route element={<PrivateRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/create-listing' element={<CreateListing />} />
-          <Route
-            path='/update-listing/:listingId'
-            element={<UpdateListing />}
-          />
-        </Route>
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/sign-in' element={<SignIn />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/listing/:listingId' element={<Listing />} />
+            <Route path='/search' element={<Search />} />
+            <Route element={<PrivateRoute />}>
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/create-listing' element={<CreateListing />} />
+              <Route path='/update-listing/:listingId' element={<UpdateListing />} />
+            </Route>
+          </Routes>
+        </main>
+        <Footer/>
+      </div>
     </BrowserRouter>
   );
 }
